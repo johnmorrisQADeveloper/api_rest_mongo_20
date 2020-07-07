109f1e6fa125d00cd0e0edf6931a571117921ee8
john morris <fennymorris@live.co.uk>
2020-07-07 20:37:02 +0100

    fix file path issue

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Todo = require('../models/Todo')
-
+const Todo = require('./models/Todo')
 // get all Todos
 router.get('/', async (req, res) => {
   try {
