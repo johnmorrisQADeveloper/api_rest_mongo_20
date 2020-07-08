f600feca46902125289a21048f7cedadc8188e06
john morris <fennymorris@live.co.uk>
2020-07-08 23:10:36 +0100

    include all crud api tests

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -1,5 +1,20 @@
-describe('todos API', () => {
-  describe('GET -', () => {
+describe('todos API', function () {
+  before(() => {
+    cy.request(
+      {
+        method: 'POST',
+        url: 'https://cryptic-sierra-47326.herokuapp.com/todos/',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: {
+          "title": `${new Date().toLocaleString()} post`,
+          "completed": false
+        }
+      }
+    ).as('postResult')
+  });
+  describe('GET -', function () {
     it('returns JSON', () => {
       cy.request('https://cryptic-sierra-47326.herokuapp.com/todos')
         .its('headers')
@@ -7,27 +22,78 @@ describe('todos API', () => {
         .should('include', 'application/json')
     })
 
-    it('loads 6 items', () => {
+    it('loads items not to be null', function () {
       cy.request('https://cryptic-sierra-47326.herokuapp.com/todos')
         .its('body')
-        .should('have.length', 6)
+        .should('not.be', null)
     })
   })
 
-  describe('GET - a specific item', () => {
-    it('returns JSON', () => {
-      cy.request('https://cryptic-sierra-47326.herokuapp.com/todos/5f008f28d7c0a62270bca547')
-        .its('body')
-        .should('include', { "title": "Buy a new phone" })
+  describe('GET - a specific item', function () {
+    it('returns JSON', function () {
+      const { body } = this.postResult
+      cy.request(`https://cryptic-sierra-47326.herokuapp.com/todos/${body._id}`)
+        .its('body').as('getSpecific')
+      cy.get('@getSpecific').then((response) => {
+        expect(response.title).to.include('post')
+        expect(response).to.have.property('completed', false)
+      })
     })
   })
 
 
-  describe('GET - 2 todos', () => {
-    it('get two items', () => {
-      cy.request('https://cryptic-sierra-47326.herokuapp.com/todos/count/2')
+  describe('GET - 1 todos', function () {
+    it('get two items', function () {
+      cy.request('https://cryptic-sierra-47326.herokuapp.com/todos/count/1')
         .its('body')
-        .should('have.length', 2)
+        .should('have.length', 1)
+    })
+  })
+
+  describe('POST - create a todo', function () {
+    it('returns JSON', function () {
+      const { body, status } = this.postResult
+      expect(status).to.equal(200)
+      expect(body.title).include('post')
+      expect(body.completed).equal(false)
+    })
+  })
+
+  describe('PATCH - update', function () {
+    it('an existing todo', function () {
+      const { body } = this.postResult
+      cy.request(
+        {
+          method: 'PATCH',
+          url: `https://cryptic-sierra-47326.herokuapp.com/todos/${body._id}`,
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: {
+            "title": 'PUT not post tired',
+            "completed": false
+          }
+        }
+      ).as('updateResult')
+      cy.get('@updateResult').then((response) => {
+        expect(response.status).equal(200)
+      })
+    })
+  })
+
+
+  describe('DELETE - todo', function () {
+    it('an existing todo', function () {
+      const { body } = this.postResult
+      cy.request(
+        {
+          method: 'DELETE',
+          url: `https://cryptic-sierra-47326.herokuapp.com/todos/${body._id}`
+        }
+      ).as('deleteResult')
+      cy.get('@deleteResult').then((response) => {
+        expect(response.status).equal(200)
+      })
     })
   })
 
